6c6be4c7e4d68ab4abdbd61b4a2b051c45b1ffde
padraic <patrick.morris.310@gmail.com>
2023-06-22 23:07:10 +0100

    update

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -42,6 +42,15 @@ let
   '';
   env = ''
     env = WLR_NO_HARDWARE_CURSORS,1
+    env = GBM_BACKEND,nvidia-drm
+    env = __GLX_VENDOR_LIBRARY_NAME,nvidia
+    env = LIBVA_DRIVER_NAME,nvidia
+    env = QT_AUTO_SCREEN_SCALE_FACTOR,1
+    env = QT_QPA_PLATFORM,wayland;xcb
+    env = XDG_CURRENT_DESKTOP,Hyprland
+    env = XDG_SESSION_TYPE,wayland
+    env = XDG_SESSION_DESKTOP,Hyprland
+    env = GDK_BACKEND=wayland,x11
     env = HYPRLAND_LOG_WLR,1
   '';
   binds = ''
