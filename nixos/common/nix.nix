d4a9bb8cbdbce57b193c284baaef02ba1bf6fdd6
padraic <patrick.morris.310@gmail.com>
2023-06-23 14:18:24 +0100

    added hyprland cachix

--- nixos/common/nix.nix
+++ nixos/common/nix.nix
@@ -24,12 +24,16 @@
       experimental-features = "nix-command flakes";
       auto-optimise-store = true;
 
-      substituters =
-        [ "https://nix-community.cachix.org" "https://cache.nixos.org" ];
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org"
+        "https://hyprland.cachix.org"
+      ];
 
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
 
       trusted-users = [ "@wheel" ];
