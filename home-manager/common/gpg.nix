28d33b8f1e66fec5effda7fff4a2bfacb1720419
padraic <patrick.morris.310@gmail.com>
2023-06-26 17:24:05 +0100

    update

--- home-manager/common/gpg.nix
+++ home-manager/common/gpg.nix
@@ -3,28 +3,28 @@
 {
   programs.gpg = {
     enable = true;
-    mutableKeys = true;
-    publicKeys = [{
-      source = ../users/padraic_pub_gpg.key;
-      trust = 5;
-    }];
+    # mutableKeys = true;
+    # publicKeys = [V
+    #   source = ../users/padraic_pub_gpg.key;
+    #   trust = 5;
+    # }];
     homedir = "${config.xdg.dataHome}/gnupg";
   };
 
-  # services.gpg-agent = {
-  #   enable = true;
-  #   enableExtraSocket = true;
-  #   enableScDaemon = true;
-  #   enableZshIntegration = true;
-  #   grabKeyboardAndMouse = true;
-  #   defaultCacheTtl = 3600;
-  #   pinentryFlavor = "gnome3";
-  #   extraConfig = ''
-  #     allow-emacs-pinentry
-  #     allow-loopback-pinentry
-  #   '';
-  #   verbose = true;
-  # };
+  services.gpg-agent = {
+    enable = true;
+    enableExtraSocket = true;
+    enableScDaemon = true;
+    enableZshIntegration = true;
+    grabKeyboardAndMouse = true;
+    defaultCacheTtl = 3600;
+    pinentryFlavor = "gnome3";
+    extraConfig = ''
+      allow-emacs-pinentry
+      allow-loopback-pinentry
+    '';
+    verbose = true;
+  };
 
   systemd.user.sessionVariables.GNUPGHOME = "${config.xdg.dataHome}/gnupg";
   # programs.zsh.sessionVariables.GNUPGHOME = "${config.xdg.dataHome}/gnupg";
