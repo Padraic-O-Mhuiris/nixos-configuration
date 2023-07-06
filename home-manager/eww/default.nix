36856cdbef31ae6aa6983f8afc58701fd4577d55
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2023-07-06 13:59:09 +0100

    update

--- home-manager/eww/default.nix
+++ home-manager/eww/default.nix
@@ -4,10 +4,10 @@
 
   home.packages = with pkgs; [ eww-wayland coreutils-full socat jq python39Full ];
 
-  home.config."eww/eww.yuck".source = ./eww.yuck;
-  home.config."eww/scripts/change-active-workspace".source = ./change-active-workspace;
-  home.config."eww/scripts/get-active-workspace".source = ./get-active-workspace;
-  home.config."eww/scripts/get-workspaces".source = ./get-workspaces;
-  home.config."eww/scripts/get-window-title".source = ./get-window-title;
+  home.configFile."eww/eww.yuck".source = ./eww.yuck;
+  home.configFile."eww/scripts/change-active-workspace".source = ./change-active-workspace;
+  home.configFile."eww/scripts/get-active-workspace".source = ./get-active-workspace;
+  home.configFile."eww/scripts/get-workspaces".source = ./get-workspaces;
+  home.configFile."eww/scripts/get-window-title".source = ./get-window-title;
 
 }
