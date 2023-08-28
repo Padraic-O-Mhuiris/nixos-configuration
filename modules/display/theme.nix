dbc8162ea729323c71ff4dba8a0c4b00f21a39c8
padraic <patrick.morris.310@gmail.com>
2023-08-28 20:29:31 +0100

    update

--- modules/display/theme.nix
+++ modules/display/theme.nix
@@ -16,4 +16,33 @@ lib.os.applyHmUsers (_: {
       size = 10;
     };
   };
-})
+}) // {
+  fonts = {
+    fontDir.enable = true;
+    enableGhostscriptFonts = true;
+    fonts = with pkgs; [
+      corefonts
+      # iosevka
+      ubuntu_font_family
+      dejavu_fonts
+      liberation_ttf
+      roboto
+      fira-code
+      jetbrains-mono
+      siji
+      font-awesome
+      cascadia-code
+      (nerdfonts.override { fonts = [ "Iosevka" ]; })
+    ];
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        monospace = [ "Iosevka NFP SemiBold" ];
+        sansSerif = [ "Roboto" ];
+        serif = [ "Roboto" ];
+      };
+    };
+  };
+
+  console.font = "Terminus 32";
+}
