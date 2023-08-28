8f31398acbf41254701153d715646a6069af7ac2
padraic <patrick.morris.310@gmail.com>
2023-08-28 20:49:21 +0100

    update

--- modules/filesystem.nix
+++ modules/filesystem.nix
@@ -1,29 +1,31 @@
 { lib, ... }:
 
 lib.os.applyHmUsers (user:
-({ config, ... }: {
-  xdg = {
-    enable = true;
-    userDirs = {
+  ({ config, pkgs, ... }: {
+    xdg = {
       enable = true;
-      createDirectories = true;
-      desktop = "${config.home.homeDirectory}/desktop";
-      download = "${config.home.homeDirectory}/downloads";
-      documents = "${config.home.homeDirectory}/documents";
-      extraConfig = { XDG_CODE_DIR = "${config.home.homeDirectory}/code"; };
-      music = "${config.home.homeDirectory}/music";
-      pictures = "${config.home.homeDirectory}/pictures";
-      publicShare = "${config.home.homeDirectory}/public";
-      templates = "${config.home.homeDirectory}/templates";
-      videos = "${config.home.homeDirectory}/videos";
+      userDirs = {
+        enable = true;
+        createDirectories = true;
+        desktop = "${config.home.homeDirectory}/desktop";
+        download = "${config.home.homeDirectory}/downloads";
+        documents = "${config.home.homeDirectory}/documents";
+        extraConfig = { XDG_CODE_DIR = "${config.home.homeDirectory}/code"; };
+        music = "${config.home.homeDirectory}/music";
+        pictures = "${config.home.homeDirectory}/pictures";
+        publicShare = "${config.home.homeDirectory}/public";
+        templates = "${config.home.homeDirectory}/templates";
+        videos = "${config.home.homeDirectory}/videos";
+      };
     };
-  };
 
-  services.udiskie = {
-    enable = true;
-    automount = true;
-    notify = true;
-    tray = "auto";
-  };
+    services.udiskie = {
+      enable = true;
+      automount = true;
+      notify = true;
+      tray = "auto";
+    };
 
-}))
+    home.packages = with pkgs; [ gnome.nautilus ];
+
+  }))
