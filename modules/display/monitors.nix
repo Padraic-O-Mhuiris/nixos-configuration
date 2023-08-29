399e6dbc21da71b110354a1db530a190154a1ff8
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2023-08-29 12:32:10 +0100

    update

--- modules/display/monitors.nix
+++ modules/display/monitors.nix
@@ -1,24 +1,6 @@
 { config, lib, pkgs, ... }:
 
-# TODO Extend to more structured config from os
-lib.os.applyHmUsers (user: {
-
-  programs.autorandr = {
-    enable = true;
-    profiles.main = {
-      fingerprint = { "DP-0" = ""; };
-      config = {
-        "DP-0" = {
-          enable = true;
-          dpi = null;
-          primary = true;
-          position = "0x0";
-          mode = "5120x1440";
-          rate = "60.00";
-        };
-      };
-    };
-  };
-
+{
   services.autorandr.enable = true;
-})
+  services.autorandr.defaultTarget = "main";
+}
