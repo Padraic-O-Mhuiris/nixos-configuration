1bd32920927e113e4514c503ed0a35fd64677f42
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2023-10-03 15:06:40 +0100

    update

--- modules/editors/emacs/default.nix
+++ modules/editors/emacs/default.nix
@@ -3,38 +3,39 @@
 let
   package = pkgs.emacsWithPackagesFromUsePackage {
     config = ""; # We don't want to create a default.el file
-    defaultInitFile = false; # pkgs.writeText "default.el" (builtins.readFile ./init.el);
+    defaultInitFile =
+      false; # pkgs.writeText "default.el" (builtins.readFile ./init.el);
     package = pkgs.emacs-unstable.override { withGTK3 = true; };
-    extraEmacsPackages = epkgs:
-      with epkgs; [
-        use-package
-        evil
-        evil-collection
-        general
-        which-key
-        command-log-mode
-        ivy
-        ivy-rich
-        counsel
-        swiper
-        doom-modeline
-        doom-themes
-        all-the-icons
-        rainbow-delimiters
-        helpful
-        hydra
-        projectile
-        counsel-projectile
-        magit
-        org
-	nix-mode
-        # forge # - is this used?
-        # (treesit-grammars.with-grammars
-        #   (g: with g; [ tree-sitter-rust tree-sitter-python ]))
-      ];
+    # extraEmacsPackages = epkgs:
+    #   with epkgs; [
+    #     use-package
+    #     evil
+    #     evil-collection
+    #     general
+    #     which-key
+    #     command-log-mode
+    #     ivy
+    #     ivy-rich
+    #     counsel
+    #     swiper
+    #     doom-modeline
+    #     doom-themes
+    #     all-the-icons
+    #     rainbow-delimiters
+    #     helpful
+    #     hydra
+    #     projectile
+    #     counsel-projectile
+    #     magit
+    #     org
+    #     nix-mode
+    #     # forge # - is this used?
+    #     # (treesit-grammars.with-grammars
+    #     #   (g: with g; [ tree-sitter-rust tree-sitter-python ]))
+    #   ];
   };
 in (lib.os.hm (user: {
-  home.file.".emacs.d/init.el".source = ./init.el;
+  # home.file.".emacs.d/init.el".source = ./init.el;
   programs.emacs = {
     inherit package;
     enable = true;
@@ -42,31 +43,30 @@ in (lib.os.hm (user: {
 
   services.emacs = {
     inherit package;
-    enable = false;
+    enable = true;
     client.enable = true;
     defaultEditor = true;
     socketActivation.enable = true;
     extraOptions = [ ];
   };
 
-  home.packages = with pkgs;
-    [
-      (ripgrep.override { withPCRE2 = true; })
-      # shellcheck
-      # shfmt
-      # pandoc
-      # fd
-      # (aspellWithDicts (dicts: with dicts; [ en en-computers en-science ]))
-      # emacsPackages.editorconfig
-      # terraform
-      # graphviz
-      # maim
-      # solc
-      # html-tidy
-      # nodePackages_latest.stylelint
-      # nodePackages_latest.js-beautify
-      # nixfmt
-    ];
+  home.packages = with pkgs; [
+    (ripgrep.override { withPCRE2 = true; })
+    shellcheck
+    shfmt
+    pandoc
+    fd
+    (aspellWithDicts (dicts: with dicts; [ en en-computers en-science ]))
+    emacsPackages.editorconfig
+    terraform
+    graphviz
+    maim
+    solc
+    html-tidy
+    nodePackages_latest.stylelint
+    nodePackages_latest.js-beautify
+    nixfmt
+  ];
 
   # stylix.targets.emacs.enable = true; # TODO Figure out how to import!
 })) // {
