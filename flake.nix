f83856ae320e9990e42a58bd7b363fc11686b586
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2023-10-16 14:48:51 +0100

    updated emacs

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
     hardware.url = "github:NixOS/nixos-hardware";
 
     emacs.url = "github:Padraic-O-Mhuiris/emacs";
+    emacs.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
