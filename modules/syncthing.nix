bd6198740f97a3611327baad2c5a1da2428d7083
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2023-11-09 20:34:41 +0000

    Update ip addresses

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -36,13 +36,13 @@
         Hydrogen = {
           id =
             "3FQY6G3-V7ECTA2-SK2CZVT-YNTOB3V-PSXVELS-CU52PLI-HLLODIG-FZCXHQH";
-          addresses = [ "tcp://192.168.0.184" ];
+          addresses = [ "tcp://192.168.0.184" "tcp://100.124.13.147" ];
           autoAcceptFolders = true;
         };
         Oxygen = {
           id =
             "DJUIYGN-RDDETOR-BHEGLE3-CPXVPB5-ICKTER2-G5ZN7BZ-MPS4RL2-JC7T5AJ";
-          addresses = [ "tcp://192.168.0.214" ];
+          addresses = [ "tcp://192.168.0.214" "tcp://100.83.242.156" ];
           autoAcceptFolders = true;
         };
       };
